67f8bb7026eea0cc991061fc633b09dd399c425c
Belén LM <113479814+bellmed22@users.noreply.github.com>
2023-10-03 21:07:05 -0300

    Add files via upload

--- Clase05/index.php
+++ Clase05/index.php
@@ -5,79 +5,55 @@ echo "<h2>Tarea Clase 5 - PD - Programación Back End</h2>" . "<h1>ESTRUCTURAS D
 // 1) Mostrar los números del 1 al 9
 echo "<h3>Números del <span>1 al 9</span></h3>";
 
-$numero = 1;
-
-while($numero <= 9){
-        echo "Esta es la iteración número: " . $numero;
+for ($i = 1; $i <= 9; $i++) {
+        echo "Esta es la iteración número: $i";
         echo "<br>";
-        $numero ++;        
-    }
+      }
+      echo "\n";
 
 // 2) Mostrar los números del 9 al 1
 echo "<h3>Números del <span>9 al 1</span></h3>";
 
-while($numero == 10){
-    $numero --;
-}
-
-while($numero >= 1){   
-        echo "Esta es la iteración número: " . $numero;
-        echo "<br>";
-        $numero -= 1;
-    }    
+for ($i = 9; $i >= 1; $i--) {
+    echo "Esta es la iteración número: $i";
+    echo "<br>";
+  }
+  echo "\n";  
 
 // 3) Mostrar los números pares del 1 al 20
 echo "<h3>Números <span>pares</span> del <span>1 al 20</span></h3>";
 
-while($numero == 0){
-        $numero += 2;
-    }
-
-while($numero <= 20){  
-        echo "Esta es la iteración número: " . $numero;
-        echo "<br>";
-        $numero += 2;
-    }       
+for ($i = 2; $i <= 20; $i += 2) {
+    echo "Esta es la iteración número: $i";
+    echo "<br>";
+  }
+  echo "\n";     
 
 // 4) Mostrar los números impares del 1 al 20
 echo "<h3>Números <span>impares</span> del <span>1 al 20</span></h3>";
 
-while($numero == 22){
-    $numero -= 21;
-}
-
-while($numero <= 20){    
-    echo "Esta es la iteración número: " . $numero;
+for ($i = 1; $i <= 20; $i += 2) {
+    echo "Esta es la iteración número: $i";
     echo "<br>";
-    $numero += 2;
-}  
+  }
+  echo "\n"; 
 
 // 5) Mostrar la suma de los números del 1 al 20
 echo "<h3>Suma de los números del <span>1 al 20</span></h3>";
+
 $suma = 0;
-
-while($numero == 21){
-    $numero -= 20;
+for ($i = 1; $i <= 20; $i++) {
+  $suma += $i;
 }
-
-while($numero <= 20){
-    $suma += $numero;
-    $numero++;  
-}  
-    echo "La suma de los números del 1 al 20 es $suma";
+echo "La suma de los números del 1 al 20 es $suma \n";
 
 
 // 6) Mostrar la suma de los números pares del 1 al 20echo "<h3>uma de los números del <span>1 al 20</span></h3>";
 echo "<h3>Suma de los <span>números pares</span>del <span>1 al 20</span></h3>";
 
-
-while($numero == 21){
-    $numero -= 21;
+$suma = 0;
+for ($i = 2; $i <= 20; $i += 2) {
+  $suma += $i;
 }
-
-while($numero <= 20){
-    $suma += $numero;
-    $numero += 2;  
-}  
-    echo "La suma de los números del 1 al 20 es $suma";
+    echo "La suma de los números pares del 1 al 20 es $suma \n";
 ?>
